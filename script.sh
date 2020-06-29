acf0a3eca86e0256edcb49211e53340e6fab4ca6
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2020-06-29 10:54:12 +0200

    preloaded-vars.conf in script

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
-cp /var/Testing/preloaded-vars.conf .
-./install.sh
+printf 'USER_LANGUAGE="en"\n
+USER_NO_STOP="y"\n
+USER_INSTALL_TYPE="server"\n
+USER_DIR="/var/ossec"\n
+USER_ENABLE_SYSCHECK="y"\n
+USER_ENABLE_ROOTCHECK="y"\n
+USER_ENABLE_OPENSCAP="y"\n
+USER_ENABLE_ACTIVE_RESPONSE="y"' > preloaded-vars.conf
 echo "Complete!"
