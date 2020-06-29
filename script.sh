d1654bf59a867420a25002d9b576319353e9102c
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2020-06-29 11:04:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
-printf 'USER_LANGUAGE="en"\n
-USER_NO_STOP="y"\n
-USER_INSTALL_TYPE="server"\n
-USER_DIR="/var/ossec"\n
-USER_ENABLE_SYSCHECK="y"\n
-USER_ENABLE_ROOTCHECK="y"\n
-USER_ENABLE_OPENSCAP="y"\n
+printf 'USER_LANGUAGE="en"
+USER_NO_STOP="y"
+USER_INSTALL_TYPE="server"
+USER_DIR="/var/ossec"
+USER_ENABLE_SYSCHECK="y"
+USER_ENABLE_ROOTCHECK="y"
+USER_ENABLE_OPENSCAP="y"
 USER_ENABLE_ACTIVE_RESPONSE="y"' > preloaded-vars.conf
 cat  preloaded-vars.conf
 ./install.sh
