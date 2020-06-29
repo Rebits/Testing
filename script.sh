183cda72d1c44d1daf2850d9b8850f2bf8710689
Víctor Rebollo <victorrebollop@gmail.com>
2020-06-29 23:25:25 +0200

    edit script

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
 cp /var/jenkins/Testing/preloaded-vars.conf ./etc/
 cat ./etc/preloaded-vars.conf
-./install.sh
+#./install.sh
 echo "Complete!"
 
 
