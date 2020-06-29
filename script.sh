332c5874cea7ca46f602ba9cca42643e5487b279
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2020-06-29 10:12:37 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
 make -C src clean
 make -C src clean-deps
-/var/ossec/bin/
+ls /var/ossec/bin/
 /var/ossec/bin/manage_agents
