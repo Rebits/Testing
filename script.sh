c5e8861635d8aaa55b4455147bee77a51b7b80c8
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2020-06-29 10:08:49 +0200

    Change systemctl to /var/ossec/bin/manage-agents

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
 make -C src clean
 make -C src clean-deps
-systemctl start wazuh-manager
+/var/ossec/bin/
+/var/ossec/bin/manage_agents
