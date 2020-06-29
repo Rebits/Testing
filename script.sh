f6723db522ccbd2b4f80afa44e5c6a090ae2a88a
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2020-06-29 10:40:53 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
-make -C src clean
-make -C src clean-deps
+./install.sh
 echo "Complete!"
-ls /var/ossec/bin/
