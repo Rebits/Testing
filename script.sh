4a9ecd41eac36bb4fab94959c73180531f132419
Víctor Rebollo <victorrebollop@gmail.com>
2020-06-29 23:00:46 +0200

    script.sh modified path preloads

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
-cp /var/jenkins/preloaded-vars.conf ./etc/
+cp /var/jenkins/Testing/preloaded-vars.conf ./etc/
 ./install.sh
 echo "Complete!"
 
