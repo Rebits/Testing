b2e4872bee394005f513a793e1168de38af6714e
Víctor Rebollo <victorrebollop@gmail.com>
2020-06-29 22:52:05 +0200

    script changed

--- script.sh
+++ script.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 curl -Ls https://github.com/wazuh/wazuh/archive/v3.13.0.tar.gz | tar zx
 cd wazuh-*
-echo 'USER_LANGUAGE="en"' >> etc/preloaded-vars.conf
-echo 'USER_NO_STOP="y"' >> etc/preloaded-vars.conf
-echo 'USER_INSTALL_TYPE="server"' >> etc/preloaded-vars.conf
-echo 'USER_DIR="/var/ossec"' >> etc/preloaded-vars.conf
-echo 'USER_ENABLE_EMAIL="n"' >> etc/preloaded-vars.conf
-echo 'USER_ENABLE_SYSCHECK="y"' >> etc/preloaded-vars.conf
-echo 'USER_ENABLE_ROOTCHECK="y"' >> etc/preloaded-vars.conf
-echo 'USER_ENABLE_OPENSCAP="y"' >> etc/preloaded-vars.conf
-echo 'USER_WHITE_LIST="n"' >> etc/preloaded-vars.conf
-echo 'USER_ENABLE_SYSLOG="y"' >> etc/preloaded-vars.conf
-echo 'USER_CA_STORE="n"' >> etc/preloaded-vars.conf
-cat  etc/preloaded-vars.conf
+cp /var/jenkins/preloaded-vars.conf ./etc/
 ./install.sh
 echo "Complete!"
 
